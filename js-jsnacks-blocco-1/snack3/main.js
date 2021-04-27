cff06ed459c24ea0bf9df0fba18eea8ca0ff1f81
dumi994 <dumi.9944@gmail.com>
2021-04-27 12:20:04 +0200

    aggironato calcolo numeri utente

--- js-jsnacks-blocco-1/snack3/main.js
+++ js-jsnacks-blocco-1/snack3/main.js
@@ -1,17 +1,11 @@
 //Snack 3:
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-var numero1 = Number(prompt('inserisci un numero'))
-var numero2 = Number(prompt('inserisci un altro numero'))
-var numero3 = Number(prompt('inserisci un altro numero'))
-var numero4 = Number(prompt('inserisci un altro numero'))
-var numero5 = Number(prompt('inserisci un altro numero'))
-var numero6 = Number(prompt('inserisci un altro numero'))
-var numero7 = Number(prompt('inserisci un altro numero'))
-var numero8 = Number(prompt('inserisci un altro numero'))
-var numero9 = Number(prompt('inserisci un altro numero'))
-var numero0 = Number(prompt('inserisci un altro numero'))
+var numero= Number(prompt('inserisci un numero'));
 
-var somma = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero0;
-console.log(numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero0);
-document.getElementById('titolo').innerHTML = 'la somma di tutti i numeri è ' + somma;
-//Il programma stampa la somma di tutti i numeri inseriti
+var somma = 0;
+
+for( var i = 0; i < 10; i++){
+    var numeroUtente = Number(prompt('inserisci un numero'))
+    somma += numeroUtente;
+}
+console.log(somma);
