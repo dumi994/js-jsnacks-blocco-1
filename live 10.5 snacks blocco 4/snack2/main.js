5248a6be6a9b0da77cfbb9d54ab691da21ae2554
dumi994 <dumi.9944@gmail.com>
2021-05-10 12:37:35 +0200

    ottenuto ipotenusa e calcolato perimetro

--- live 10.5 snacks blocco 4/snack2/main.js
+++ live 10.5 snacks blocco 4/snack2/main.js
@@ -9,7 +9,10 @@ console.log(triangoloRett);
 //Calcolare perimetro e area.
 var area = triangoloRett.base * triangoloRett.altezza / 2;
 console.log("l'area del rettangolo è " + area);
-var perimetro = (triangoloRett.base *2) + (triangoloRett.altezza * 2);
+var ipotenusa = ((area * 2) / triangoloRett.altezza);
+console.log('l ipotenusa è ' + ipotenusa);
+
+var perimetro = triangoloRett.base + triangoloRett.altezza + ipotenusa;
 console.log("il perimetro del rettangolo è " + perimetro);
 
 
