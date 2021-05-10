5e0f6e36f91d40e6d5364400a7a0278efadd79b8
dumi994 <dumi.9944@gmail.com>
2021-05-10 12:30:44 +0200

    calcolata l'area del triangolo

--- live 10.5 snacks blocco 4/snack2/main.js
+++ live 10.5 snacks blocco 4/snack2/main.js
@@ -1,15 +1,15 @@
 //Blocco 4 - Sanck 2
 //Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
-var rettangolo = {
+var triangoloRett = {
     base: 20,
     altezza: 16, 
 };
-console.log(rettangolo);
+console.log(triangoloRett);
 
 //Calcolare perimetro e area.
-var area = rettangolo.base * rettangolo.altezza;
+var area = triangoloRett.base * triangoloRett.altezza / 2;
 console.log("l'area del rettangolo è " + area);
-var perimetro = (rettangolo.base *2) + (rettangolo.altezza * 2);
+var perimetro = (triangoloRett.base *2) + (triangoloRett.altezza * 2);
 console.log("il perimetro del rettangolo è " + perimetro);
 
 
