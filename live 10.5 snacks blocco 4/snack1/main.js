529ba48b46b942ae9b3a8602b5d7f5d0e0050dfc
dumi994 <dumi.9944@gmail.com>
2021-05-10 11:45:33 +0200

    modificato il peso tramite prompt

--- live 10.5 snacks blocco 4/snack1/main.js
+++ live 10.5 snacks blocco 4/snack1/main.js
@@ -1,7 +1,16 @@
+//1a:
+//Creare un oggetto palla che abbia le seguenti proprietà.
+
 var palla = {
     'nome':'palla',
-    peso: '10',
+    peso: 10,
 }
 for (var key in palla){
     console.log(palla[key]);
 }
+
+//Snack 1B
+//Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+var modificaPeso = prompt('Definisci il nuovo peso della palla')
+palla.peso = modificaPeso;
+console.log('il nuovo peso è ' + palla.peso);
